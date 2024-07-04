45d73709414109b5a7c2bc39875b053943950b5c
Virgarakha <142412561+Virgarakha@users.noreply.github.com>
2024-07-04 18:46:22 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+// Data PDN Jaga baik baik dong kominfo
+// Ada di sini linknya \/
+
+// Data.html
+
+// lali carae?
+// contoh
+// https://google.com/login.html
+// garek ganti login.html e
+// jadi Data.html
+// gor contoh iki
